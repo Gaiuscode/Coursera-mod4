053d6f342678d4676f636dbd5d7f74b31a4d054c
GaiusCode <boluwatifegaius@gmail.com>
2022-11-03 22:09:12 +0100

    fixed it up

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 (function () {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Gaius", "John", "Jude", "Jerry", "Odogwu", "Freeman", "Peter", "Paula", "Mufu", "Jim"];
     for (var i = 0; i < names.length; i++) {
       var firstLetter = names[i].charAt(0).toLowerCase();
       if (firstLetter === 'j') {
