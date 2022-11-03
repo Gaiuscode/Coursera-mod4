d8e2cf08fa9f1f20eee4f85f45cb6ae78ff89b45
GaiusCode <boluwatifegaius@gmail.com>
2022-11-03 21:05:07 +0100

    fixed it up

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,3 +1,8 @@
 (function (window) {
 	var helloSpeaker = {};
 	var speakWord = "Hello";
+    helloSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name);
+	}
+	window.helloSpeaker = helloSpeaker;
+})(window);
